06cc615a629009dab42a3a639f0f04da74459ce7
Lavin Lavin <123382028+lavinlavi@users.noreply.github.com>
2023-08-22 19:19:11 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -22,6 +22,10 @@ int _printf(const char *format, ...)
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
+		if (format[i] == % && format[i + 1] == '\0')
+		{
+			return (-1);
+		}
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '%')
